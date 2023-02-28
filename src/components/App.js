25b0778afb79bdcb604a476e7d7dfff81256a6fe
Iktan <delav1004@gmail.com>
2023-02-28 17:26:08 -0600

    Step 1 Initializing Trget Sum app. -Importing App from App.js -Adding a text and styles

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import {View, Text, StyleSheet} from 'react-native';
+
+class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#ddd',
+    flex: 1,
+  },
+});
+
+export default App;
