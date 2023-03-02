fe68bdffd6608d1873ee4b5512a086c8b6e248ed
Iktan <delav1004@gmail.com>
2023-03-02 03:09:48 -0600

    Step 11 Changing UI based on game status - Making colour change depending on game status( won=green, lost=red). - Disable numbers when game is already won or lost.

--- src/components/Game.js
+++ src/components/Game.js
@@ -49,14 +49,18 @@ class Game extends React.Component {
     const gameStatus = this.gameStatus();
     return (
       <View style={styles.container}>
-        <Text style={styles.target}>{this.target}</Text>
+        <Text style={[styles.target, styles[`STATUS_${gameStatus}`]]}>
+          {this.target}
+        </Text>
         <View style={styles.randomContainer}>
           {this.randomNumbers.map((randomNumber, index) => (
             <RandomNumber
               key={index}
               id={index}
               number={randomNumber}
-              isDisabled={this.isNumberSelected(index)}
+              isDisabled={
+                this.isNumberSelected(index) || gameStatus !== 'PLAYING'
+              }
               onPress={this.selectNumber}
             />
           ))}
@@ -92,6 +96,15 @@ const styles = StyleSheet.create({
     marginVertical: 25,
     fontSize: 35,
     textAlign: 'center',
+  },
+  STATUS_PLAYING: {
+    backgroundColor: '#bbb',
+  },
+  STATUS_WON: {
+    backgroundColor: 'green',
+  },
+  STATUS_LOST: {
+    backgroundColor: 'red',
   },
 });
 
